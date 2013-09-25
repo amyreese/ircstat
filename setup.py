38267a29b7ba98b5fa11fda909f4274a480a3dff
John Reese <john@noswap.com>
2013-09-24 18:05:20 -0700

    pypi/travis framework

---
+++ setup.py
@@ -0,0 +1,30 @@
+from setuptools import setup
+
+from os import path
+import shutil
+
+import ircstat
+
+if path.isfile('README.md'):
+    shutil.copyfile('README.md', 'README')
+
+setup(name='ircstat',
+      description='generate statistics and graphs from IRC channel logs',
+      version=ircstat.VERSION,
+      author='John Reese',
+      author_email='john@noswap.com',
+      url='https://github.com/jreese/ircstat',
+      classifiers=['License :: OSI Approved :: MIT License',
+                   'Operating System :: OS Independent',
+                   'Programming Language :: Python',
+                   'Programming Language :: Python :: 2.7',
+                   'Programming Language :: Python :: 3',
+                   'Programming Language :: Python :: 3.2',
+                   'Programming Language :: Python :: 3.3',
+                   'Topic :: Utilities',
+                   'Development Status :: 2 - Pre-Alpha',
+                   ],
+      license='MIT License',
+      packages=['ircstat'],
+      scripts=['bin/ircstat'],
+      )

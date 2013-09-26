2ca101e70076c7745d6ab34573ef266789be2435
John Reese <john@noswap.com>
2013-09-25 18:27:26 -0700

    Add basic entrypoint method

--- ircstat/__init__.py
+++ ircstat/__init__.py
@@ -1,6 +1,6 @@
 # Copyright 2013 John Reese
 # Licensed under the MIT license
 
-from .ircstat import VERSION
+from .ircstat import VERSION, do_everything
 from .log import logger
 from .config import load_config
--- ircstat/ircstat.py
+++ ircstat/ircstat.py
@@ -2,3 +2,8 @@
 # Licensed under the MIT license
 
 VERSION = '0.1.0'
+
+def do_everything(input_path, output_path, config):
+    """One entry point to rule them all."""
+
+    pass

d886afea13973e9d1b4d3a0323dc25ab2e41bb7a
John Reese <john@noswap.com>
2013-09-25 19:01:08 -0700

    Added basic Struct object

--- ircstat/__init__.py
+++ ircstat/__init__.py
@@ -1,6 +1,7 @@
 # Copyright 2013 John Reese
 # Licensed under the MIT license
 
+from .ent import Struct
 from .ircstat import VERSION, do_everything
 from .log import logger
 from .config import load_config
---
+++ ircstat/ent.py
@@ -0,0 +1,13 @@
+# Copyright 2013 John Reese
+# Licensed under the MIT license
+
+class Struct(object):
+    """A basic object type that, given a dictionary or keyword arguments,
+    converts the key/value pairs into object attributes."""
+    def __init__(self, data=None, **kwargs):
+        if data is not None:
+            self.__dict__.update(data)
+        self.__dict__.update(kwargs)
+
+    def __repr__(self):
+        return '<Struct %s>' % self.__dict__

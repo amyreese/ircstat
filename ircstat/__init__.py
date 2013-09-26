5fd4caf81bbe850d8d57f0b29e221ec8c0e2e910
John Reese <john@noswap.com>
2013-09-26 16:18:33 -0700

    Clean up ircstat namespace

--- ircstat/__init__.py
+++ ircstat/__init__.py
@@ -1,8 +1,6 @@
 # Copyright 2013 John Reese
 # Licensed under the MIT license
 
-from .ent import Struct
 from .ircstat import VERSION, do_everything
 from .log import logger
 from .config import load_config
-from .parser import LogParser

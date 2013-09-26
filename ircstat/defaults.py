f7f065054765ffa7a3b9680228f04b77bdb47824
John Reese <john@noswap.com>
2013-09-25 18:13:18 -0700

    Add default config values

---
+++ ircstat/defaults.py
@@ -0,0 +1,8 @@
+# Copyright 2013 John Reese
+# Licensed under the MIT license
+
+filename_regex = r'(?:[a-z]+_)#(?P<channel>[a-z]+)_(?P<date>\d{8}).log'
+
+channel_regex_group = 1
+date_regex_group = 2
+date_format = r'%Y%m%d'

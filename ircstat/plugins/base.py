74d0d42a909b95ead51d26c0173c09f5e466e1a2
John Reese <john@noswap.com>
2013-09-26 19:13:27 -0700

    Basic plugin call framework

--- ircstat/plugins/base.py
+++ ircstat/plugins/base.py
@@ -1,7 +1,9 @@
 # Copyright 2013 John Reese
 # Licensed under the MIT license
 
-from ..ent import Struct
+import re
+
+from ..ent import Struct, NetworkStat
 from ..log import logger
 
 log = logger(__name__)
@@ -12,3 +14,30 @@ class Plugin(Struct):
         Struct.__init__(self,
                         config=config,
                         **config.plugins.__dict__[self.name].__dict__)
+
+    def process(self, conversations):
+        """Process a list of conversations and return a NetworkStat object."""
+        network = NetworkStat()
+
+        for channel in conversations:
+            channel_stats = network.channels[channel]
+
+            for date, conversation in conversations[channel].items():
+                daily_stats = channel_stats.days[date]
+
+                self.process_conversation(conversation,
+                                          network, channel_stats, daily_stats)
+
+        return network
+
+    def process_conversation(self, conversation, network, channel, day):
+        """Process a single conversation, with the given network, channel, and
+        daily statistics objects."""
+
+        for message in conversation.messages:
+            self.process_message(message, network, channel, day)
+
+    def process_message(self, message, network, channel, day):
+        """Process an individual message, with the given network, channel, and
+        daily statistics objects."""
+        raise NotImplementedError()

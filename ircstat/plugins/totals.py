6d26014321ad7828920b498e0ba23bcad49224ff
John Reese <john@noswap.com>
2013-09-27 17:17:42 -0700

    Add word count plugin

--- ircstat/plugins/totals.py
+++ ircstat/plugins/totals.py
@@ -1,6 +1,7 @@
 # Copyright 2013 John Reese
 # Licensed under the MIT license
 
+from ..lib import *
 from ..ent import Message
 from .base import Plugin
 
@@ -13,3 +14,17 @@ class Totals(Plugin):
         mtype = Message.type_to_name(message.type)
 
         self.inc_shared_stats(nick, mtype=1)
+
+class Wordcount(Plugin):
+    """Tracks the average word count of messages."""
+    def process_message(self, message):
+        if is_bot(message) or not message.content:
+            return
+
+        nick = message.nick
+        word_count = len(message.content.split())
+
+        self.inc_shared_stats(nick,
+                              word_count_total=word_count,
+                              word_count_messages=1,
+                              )
